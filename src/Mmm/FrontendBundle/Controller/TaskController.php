f80544ddf40049a2139d39df33e3e964e9baac40
Marcin <marcin@mbosweb.pl>
2015-03-08 15:11:56 +0100

    Dodanie widoku dla dodawania tasków

--- src/Mmm/FrontendBundle/Controller/TaskController.php
+++ src/Mmm/FrontendBundle/Controller/TaskController.php
@@ -1,18 +1,50 @@
 <?php
 
 namespace Mmm\FrontendBundle\Controller;
-
+use Mmm\FrontendBundle\Entity\Task;
+use Mmm\FrontendBundle\Form\TaskType;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Template;
+use Symfony\Component\HttpFoundation\Request;
 
+/**
+ * Class UserController
+ * @package Mmm\FrontendBundle\Controller
+ * @Route("/task")
+ * @Template()
+ */
 class TaskController extends Controller
 {
-    /**
-     * @Route("/tasks")
-     */
-    public function showTasksAction() {
 
+    /**
+     * @Route("/", name="_aaa_task_list")
+     */
+    public function indexAction()
+    {
         return $this->render("MmmFrontendBundle:Task:index.html.twig");
     }
+
+    /**
+     * @Route("/add", name="_aaa_task_add")
+     */
+    public function addAction(Request $request)
+    {
+        $task = new Task();
+
+        $form = $this->createForm(new TaskType(), $task);
+        $form->handleRequest($request);
+
+        if ($form->isValid()) {
+            $em = $this->getDoctrine()->getManager();
+
+            $em->persist($task);
+            $em->flush($task);
+        }
+
+        return array(
+            'form' => $form->createView()
+        );
+    }
+
 }
