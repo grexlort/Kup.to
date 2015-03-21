b289f2881b6c610030529a3f6a0df7e5e2ec4f09
tomasz_kulka <kulka.tomek@gmail.com>
2015-03-21 11:08:41 +0100

    Dodanie seterów i geterów do entitki

--- src/Mmm/FrontendBundle/Controller/DefaultController.php
+++ src/Mmm/FrontendBundle/Controller/DefaultController.php
@@ -4,8 +4,6 @@ namespace Mmm\FrontendBundle\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
-use Sensio\Bundle\FrameworkExtraBundle\Configuration\Template;
-use Sensio\Bundle\FrameworkExtraBundle\Configuration\Security;
 
 class DefaultController extends Controller
 {
--- src/Mmm/FrontendBundle/Controller/UserController.php
+++ src/Mmm/FrontendBundle/Controller/UserController.php
@@ -88,6 +88,4 @@ class UserController extends Controller
 
         return $this->redirect('/');
     }
-
-
 }
