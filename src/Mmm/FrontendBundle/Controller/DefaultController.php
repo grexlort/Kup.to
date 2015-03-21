12446caff8973754a7d76f9d5e701511d22dfa81
Marcin <marcin@mbosweb.pl>
2015-03-21 11:34:17 +0100

    przekierowanie dla niezalogowanych

--- src/Mmm/FrontendBundle/Controller/DefaultController.php
+++ src/Mmm/FrontendBundle/Controller/DefaultController.php
@@ -8,7 +8,7 @@ use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 class DefaultController extends Controller
 {
     /**
-     * @Route("/test")
+     * @Route("/")
      */
     public function indexAction()
     {
